3772028f8a80293eb914b2a1161d21f87edf12d0
xiva-wgt <xiva.wgt@gmail.com>
2017-02-07 23:50:40 +0300

    site
    first commit

---
+++ js/helpers.js
@@ -0,0 +1,21 @@
+function popap() {
+    alert('Yeapp!');
+}
+
+function init() {
+    $(document).ready(function() {
+        $(".fancybox-thumb").fancybox({
+            prevEffect	: 'none',
+            nextEffect	: 'none',
+            helpers	: {
+                title	: {
+                    type: 'outside'
+                },
+                thumbs	: {
+                    width	: 50,
+                    height	: 50
+                }
+            }
+        });
+    });
+}
